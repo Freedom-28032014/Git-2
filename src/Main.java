2e3b92e8877adbffbaf597bca77e05c709ba2d1f
Tanya <tanyalykina57@gmail.com>
2022-08-27 21:09:28 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager salesManager = new SalesManager(new int[]{});
+        int max = salesManager.max();
+        System.out.println(max);
+    }
+}
